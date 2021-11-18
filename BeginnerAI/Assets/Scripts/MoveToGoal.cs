50afdce6510b72c95bab734765c3b7d4978f399a
Aditya Galang Adzana <adityagalang80@gmail.com>
2021-11-18 15:07:48 +0700

    Section 9

---
+++ BeginnerAI/Assets/Scripts/MoveToGoal.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveToGoal : MonoBehaviour
+{
+    public float speed = 2.0f;
+    public float accuracy = 0.01f;
+    public Transform goal;
+    // Start is called before the first frame update
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.LookAt(goal.position);
+        Vector3 direction = goal.position - this.transform.position;
+        if(direction.magnitude > accuracy)
+        this.transform.Translate(direction.normalized * speed * Time.deltaTime,Space.World);
+    }
+}
