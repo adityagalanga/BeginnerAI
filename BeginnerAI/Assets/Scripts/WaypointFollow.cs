89c12ef214f58d6553a733a0acdcdbaae1eced3d
Aditya Galang Adzana <adityagalang80@gmail.com>
2021-11-18 16:01:13 +0700

    Waypoint with tracker

--- BeginnerAI/Assets/Scripts/WaypointFollow.cs
+++ BeginnerAI/Assets/Scripts/WaypointFollow.cs
@@ -4,25 +4,25 @@ using UnityEngine;
 
 public class WaypointFollow : MonoBehaviour
 {
-    public GameObject[] waypoints;
+    public UnityStandardAssets.Utility.WaypointCircuit circuit;
     int currentWP = 0;
 
-    float speed = 1.0f;
+    float speed = 5.0f;
     float accuracy = 1.0f;
-    float rotSpeed = 0.4f;
+    float rotSpeed = 1f;
 
     // Start is called before the first frame update
     void Start()
     {
-        waypoints = GameObject.FindGameObjectsWithTag("waypoint");
+        //waypoints = GameObject.FindGameObjectsWithTag("waypoint");
     }
 
     // Update is called once per frame
     void LateUpdate()
     {
-        if (waypoints.Length == 0) return;
+        if (circuit.Length == 0) return;
 
-        Vector3 lookAtGoal = new Vector3(waypoints[currentWP].transform.position.x, this.transform.position.y, waypoints[currentWP].transform.position.z);
+        Vector3 lookAtGoal = new Vector3(circuit.Waypoints[currentWP].transform.position.x, this.transform.position.y, circuit.Waypoints[currentWP].transform.position.z);
 
         Vector3 direction = lookAtGoal - this.transform.position;
         this.transform.rotation = Quaternion.Slerp(this.transform.rotation, Quaternion.LookRotation(direction), Time.deltaTime * rotSpeed);
@@ -30,7 +30,7 @@ public class WaypointFollow : MonoBehaviour
         if (direction.magnitude < accuracy)
         {
             currentWP++;
-            if(currentWP >= waypoints.Length)
+            if(currentWP >= circuit.Waypoints.Length)
             {
                 currentWP = 0;
             }
